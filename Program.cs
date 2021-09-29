9e38cfb30f48c34a9f4262861338a45d1465bd6e
Павел Семейко <pawel.semeiko@yandex.ru>
2021-09-29 18:53:46 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PeregruzMetpoiskanaib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            metodPoiska metodpoiska = new metodPoiska();
+           int alfa = metodpoiska.MetodBogooW(15, 13, 55, 33, 67, 77, 444, 999);
+            Console.WriteLine(alfa);
+            int beta = metodpoiska.MetodPoiska(13, 19, 122);
+            Console.WriteLine(beta);
+            string sigma = metodpoiska.MetodBogooS("dasda", "fsdffwe", "fsdfegwgg", "ds");
+            Console.WriteLine(sigma);
+        }
+    }
+}
---
+++ metodPoiska.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeregruzMetpoiskanaib
+{
+    class metodPoiska
+    {
+
+        public int MetodPoiska(int a, int b, int c)
+        {
+            int[] abc = new int[] { a, b, c };
+            int result = abc[0];
+
+            for (int i = 0; i < abc.Length; i++)
+            {
+                if (result < abc[i])
+                {
+                    result = abc[i];
+                }
+            }
+            return result;
+        }
+        // Метод для поиска найбольшего значения из трех данных значений. При 2 или 4 значениях работать не будет.
+        // Метод лохов так сказать
+        // но если потребуется на 4 числа или пять. просто дописывай в первую строку еще одно значение инт.
+        // И добавляй в массиве их {} но все это, опять же для лохов
+        // Ибо чмошники не знают ключевого слова " params "
+        // оно позволяет зделать перегрузку метода с неограниченым числом параметров ОДНОГО типа
+        public int MetodBogooW(params int[] aboba)
+        {
+            int result = aboba[0];
+
+            for (int i = 0; i < aboba.Length; i++)
+            {
+                if (result < aboba[i])
+                {
+                    result = aboba[i];
+                }
+            }
+            return result;
+        }
+        /// видишь как нехуйски упрощает всё. МОжешь  пихать сколько угодно элементов. И массив тут нужен именно для этого.
+        /// потому что мы не знаем сколько элементов будет вводить человек. 
+        /// но они будут автоматом вписываться. 5-массив из 5 и 10 массив из 10. 
+        public string MetodBogooS(params string[] aboba)
+        {
+            string result = aboba[0];
+
+            for (int i = 0; i < aboba.Length; i++)
+            {
+                if (result.Length < aboba[i].Length)
+                {
+                    result = aboba[i];
+                }
+            }
+            return result;
+        }
+    }
+}
